1e4b0421645bf4c5ab51e82d86ee6242a9e056f7
Elaine van Bergen <elvanber@microsoft.com>
2017-03-08 15:00:08 +1100

    Ugly code for PowerBI

--- CoLabDashboard/wwwroot/js/site.js
+++ CoLabDashboard/wwwroot/js/site.js
@@ -1,1 +1,14 @@
-﻿// Write your Javascript code.
+﻿$(() => {
+    var $reportContainer = $('#reportContainer');
+
+    var reportUrl = 'http://powerbipaasapi.azurewebsites.net/api/reports/63f50faa-f1fe-40ed-ab33-67fb09b80251';
+
+    fetch(reportUrl)
+        .then(response => response.json())
+        .then(report => {
+            var reportConfig = $.extend({ type: 'report' }, report);
+            $reportContainer.powerbi(reportConfig);
+        });
+});
+
+// Write your Javascript code.
